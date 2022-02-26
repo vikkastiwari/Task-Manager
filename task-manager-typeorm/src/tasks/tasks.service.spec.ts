bde44e34a8e916bbbd6d48cacab99e0186f398b1
vikas <v8kgsm@gmail.com>
2022-02-26 22:17:52 +0530

    testing

--- task-manager-typeorm/src/tasks/tasks.service.spec.ts
+++ task-manager-typeorm/src/tasks/tasks.service.spec.ts
@@ -1,6 +1,10 @@
+import { GetTaskFilterDto } from './dto/get-filtered-tasks.dto';
+import { TaskStatus } from './task-status.enum';
 import { TaskRepository } from './task.repository';
 import { TasksService } from './tasks.service';
-import {Test} from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+const mockUser = { username: 'Test user' };
 
 const mockTaskRepository = () => ( {
     // mock function
@@ -25,8 +29,26 @@ describe('TasksService',()=>{
     } )
     
     describe( 'getTasks', () => {
-        it( 'get all tasks from the repository', () => {
+        it( 'get all tasks from the repository', async() => {
+
+            // 
+            // taskRepository.getTasks.mockRejectedValue();
+            // taskRepository.getTasks.mockReturnedValue();
+
+            // we are using this coz it is returning the promise
+            taskRepository.getTasks.mockResolvedValue('someValue');
+
             expect( taskRepository.getTasks ).not.toHaveBeenCalled();
+
+            const filters: GetTaskFilterDto = { status: TaskStatus.IN_PROGRESS, search: 'some search query' };
+
+            // call getTasks()
+            const result = await tasksService.getTasks( filters, mockUser );
+
+            // checking on called function
+            expect( taskRepository.getTasks ).toHaveBeenCalled();
+
+            expect( result ).toBe( 'someValue' );
         })
     })
 })
