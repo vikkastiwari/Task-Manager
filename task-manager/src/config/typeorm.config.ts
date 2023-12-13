1b0e2b936494ae3ec500553a9be15e2512aaa0f1
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2023-12-13 14:24:05 +0530

    Update typeorm.config.ts

--- task-manager/src/config/typeorm.config.ts
+++ task-manager/src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ console.log(process.env.database);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    url: "postgres://urhuhujw:7VF9560KOVlW1f7FCdUW1YbHoBrzgVv3@castor.db.elephantsql.com/urhuhujw",
+    url: "url",
     autoLoadEntities: true,
     synchronize: true,
 }
